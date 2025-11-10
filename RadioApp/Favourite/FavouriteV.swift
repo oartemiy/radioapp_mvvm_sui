9bf0cac8758c35340405bae1e9dbf60a607cbfab
oartemiy <artemiy.obraztsov@yandex.ru>
2025-11-10 14:00:45 +0300

    4 points out of 10 are ready

---
+++ RadioApp/Favourite/FavouriteV.swift
@@ -0,0 +1,14 @@
+//
+//  FavouriteV.swift
+//  RadioApp
+//
+//  Created by Артемий Образцов on 09.11.2025.
+//
+
+import SwiftUI
+
+struct FavouriteV: View {
+    var body: some View {
+        
+    }
+}
